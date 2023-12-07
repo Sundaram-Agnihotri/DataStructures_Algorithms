99f180399c292c1a1dd2d93206469de06efce1cc
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-12-07 15:37:49 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        sort(arr,arr+n);
+        
+      
+        int l = 0;
+        int r = n-1;
+        
+        while(l<=r){
+        
+        int mid = (l+r)/2;
+        
+        if(arr[mid] == k)
+        return mid;
+        
+        else if(arr[mid] < k){
+            l = mid+1;
+        }
+        else
+        {
+            r = mid-1;
+        }
+        }
+        
+        return -1;
+    }
+};
