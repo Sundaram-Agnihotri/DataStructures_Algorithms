1c2813ed6a120065c1b52f5944d413d58a9579c6
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2023-12-08 20:21:47 +0530

    Create insertionSort.cpp

---
+++ insertionSort.cpp
@@ -0,0 +1,14 @@
+void insertionSort(int arr[], int n)
+    {
+        //code here
+        for(int i=0;i<n;i++){
+            int j = i;
+            
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
