94626337d0d37103e8bdcc86d0cdf11ee717c5dc
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-02-02 08:36:05 +0530

    Create  Editorial Editorial Solutions Solutions Submissions Code Testcase Test Result Test Result 17. Letter Combinations of a Phone Number.cpp

---
+++ Editorial Editorial Solutions Solutions Submissions Code Testcase Test Result Test Result 17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+
+    private: 
+
+    void solve(string digit,string output,int index,vector<string>&ans,string mapping[])
+    {
+        if(index>=digit.length())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        int number = digit[index] - '0';
+        string value = mapping[number];
+
+        for(int i=0;i<value.length();i++)
+        {
+            output.push_back(value[i]);
+
+            solve(digit,output,index+1,ans,mapping);
+
+            output.pop_back();
+        }
+    }
+public:
+    vector<string> letterCombinations(string digit) {
+
+        vector<string>ans;
+
+        if(digit.length() == 0)
+        return ans;
+
+        string output;
+        int index = 0;
+
+        string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        solve(digit,output,index,ans,mapping);
+
+        return ans;
+    }
+};
